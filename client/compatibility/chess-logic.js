60107d6783773ba1644a11ab80d2789ee3a5fa73
Lucas McCain <lucasprograms@gmail.com>
2015-11-24 21:14:29 -0500

    chess

---
+++ client/compatibility/chess-logic.js
@@ -0,0 +1,70 @@
+
+  var game, currentGameId, currentGame;
+  
+  var board,
+      statusEl = $('#status'),
+      fenEl = $('#fen'),
+      pgnEl = $('#pgn');
+
+
+  var onDragStart = function(source, piece, position, orientation) {
+  
+    if (game.game_over() === true ||
+        (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+      return false;
+    }
+  };
+
+  var onDrop = function(source, target) {
+    // see if the move is legal
+    
+    var move = game.move({
+      from: source,
+      to: target,
+      promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    });
+
+    // illegal move
+    if (move === null) return 'snapback';
+    
+    Games.update(Meteor.users.findOne(Meteor.userId()).profile.currentGame, {$set: {fen: game.fen()}})
+    updateStatus();
+  };
+
+// update the board position after the piece snap 
+// for castling, en passant, pawn promotion
+  var onSnapEnd = function() {
+    board.position(game.fen());
+  };
+
+  var updateStatus = function() {
+    var status = '';
+
+    var moveColor = 'White';
+    if (game.turn() === 'b') {
+      moveColor = 'Black';
+    }
+
+    // checkmate?
+    if (game.in_checkmate() === true) {
+      status = 'Game over, ' + moveColor + ' is in checkmate.';
+    }
+
+    // draw?
+    else if (game.in_draw() === true) {
+      status = 'Game over, drawn position';
+    }
+
+    // game still on
+    else {
+      status = moveColor + ' to move';
+
+      // check?
+      if (game.in_check() === true) {
+        status += ', ' + moveColor + ' is in check';
+      }
+    }
+
+    statusEl.html(status);
+  };
---
+++ meteor-chess.js
@@ -0,0 +1,86 @@
+Games = new Mongo.Collection('games');
+
+NEW_GAME_FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+
+if (Meteor.isClient) {
+  Meteor.subscribe('allUsers');
+  Meteor.subscribe('allGames');
+
+  Template.game.helpers({
+    'userHasGame': function userHasGame() {
+      return !!Meteor.user().profile.currentGame;
+    },
+
+    'currentGame': function getCurrentGame() {
+      currentGame = Games.findOne(Meteor.users.findOne(Meteor.userId())
+                                              .profile.currentGame);
+      debugger
+      game = new Chess(currentGame.fen);
+
+      if (currentGame) {
+        let cfg = {
+          draggable: true,
+          position: currentGame.fen,
+          onDragStart: onDragStart,
+          onDrop: onDrop,
+          onSnapEnd: onSnapEnd,
+        };
+
+        statusEl = $('#status');
+        board = ChessBoard('board', cfg);
+
+        updateStatus();
+      }
+    },
+  });
+
+  Template.game.events({
+    'click .newGame': function newGame() {
+      newGameId = new Meteor.Collection.ObjectID();
+
+      Meteor.call('newGame', newGameId);
+      Meteor.call('updateUserGame', Meteor.userId(), newGameId);
+    },
+  });
+
+  Template.social.events({
+    'submit .inviteFriendToGame': function inviteFriendToGame(e) {
+      e.preventDefault();
+      let email = $(e.currentTarget).find('input');
+      let user = Meteor.users.findOne({'emails.address': email.val()});
+
+      Meteor.call('updateUserGame', user._id, Meteor.users
+            .findOne(Meteor.userId()).profile.currentGame);
+
+      email.val('');
+    },
+  });
+}
+
+Meteor.methods({
+  newGame: function newGame(newGameId) {
+    Games.insert({
+      _id: newGameId,
+      fen: NEW_GAME_FEN,
+      player1: Meteor.userId(),
+    });
+  },
+
+  updateUserGame: function updateUserGame(userId, gameId) {
+    Meteor.users.update(userId, {$set: {profile: {currentGame: gameId}}});
+  },
+});
+
+
+if (Meteor.isServer) {
+  Meteor.publish('allUsers', function publishUsers() {
+    return Meteor.users.find({});
+  });
+
+  Meteor.publish('allGames', function publishGames() {
+    return Meteor.games.find({});
+  });
+}
+
+// in chess min line 304
+// history:function(r){for(var e=[],n=[],t=("undefined"!=typeof r&&"verbose"in r&&r.verbose);vr.length>0;)e.push(m());for(;e.length>0;){var o=e.pop();n.push(t?O(o):v(o)),y(o)}return n}
