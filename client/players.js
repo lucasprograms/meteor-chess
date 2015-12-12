f436992ce71bc7a2f04a0df4e4e824d78ab396fe
Lucas McCain <lucasprograms@gmail.com>
2015-12-12 18:36:15 -0500

    add opponent name

--- client/players.js
+++ client/players.js
@@ -1,13 +1,17 @@
-Template.currentOpponent.helpers({
-  currentOpponent: function getCurrentOpponent() {                  
-    if (currentOpponent) {
-      return currentOpponent.username;
-    }
-  },
-});
-
-Template.currentPlayer.helpers({
+Template.gameStatus.helpers({
   currentPlayer: function getCurrentPlayer() {
     return Meteor.user().username;
+  },
+
+  currentOpponent: function getCurrentOpponent() {
+    let currentOpp = null;
+
+    Meteor.user().currentGames.forEach( function getCorrectOpponent(game) {
+      if (game.game._str === Meteor.user().profile.currentGame._str) {
+        currentOpp = game.currentOpponent;
+      }
+    });
+
+    return currentOpp;
   },
 });
